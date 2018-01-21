8c657de392f4b3306f7dcb6eff4c25f4e252e0e9
dozonot <31640715+dozonot@users.noreply.github.com>
2018-01-21 16:02:07 +0900

    Update vmclone.bat

--- vmclone.bat
+++ vmclone.bat
@@ -1,6 +1,6 @@
 set VMRUNPATH=C:\Program Files (x86)\VMware\VMware Workstation\
 set VMDIR=%userprofile%\Documents\Virtual Machines
 set SRCVM=CentOS7-minimal
-set TGTVM=Linked-Clone05
+set TGTVM=Linked-Clone06
 "%VMRUNPATH%vmrun.exe" -T ws clone "%VMDIR%\%SRCVM%\%SRCVM%.vmx" "%VMDIR%\%TGTVM%\%TGTVM%.vmx" linked -cloneName="%TGTVM%"
-"%VMRUNPATH%vmrun.exe" start "%VMDIR%\%TGTVM%\%TGTVM%.vmx"
+"%VMRUNPATH%vmrun.exe" -T ws start "%VMDIR%\%TGTVM%\%TGTVM%.vmx"
