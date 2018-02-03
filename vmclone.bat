00c3c4326a58a1fe266c0ecd4f019aae6b6a8e13
dozonot <31640715+dozonot@users.noreply.github.com>
2018-02-03 19:33:52 +0900

    change variable name, and add snapshot name.

--- vmclone.bat
+++ vmclone.bat
@@ -1,6 +1,7 @@
 set VMRUNPATH=C:\Program Files (x86)\VMware\VMware Workstation\
 set VMDIR=%userprofile%\Documents\Virtual Machines
-set SRCVM=CentOS7-minimal
-set TGTVM=Linked-Clone06
-"%VMRUNPATH%vmrun.exe" -T ws clone "%VMDIR%\%SRCVM%\%SRCVM%.vmx" "%VMDIR%\%TGTVM%\%TGTVM%.vmx" linked -cloneName="%TGTVM%"
-"%VMRUNPATH%vmrun.exe" -T ws start "%VMDIR%\%TGTVM%\%TGTVM%.vmx"
+set SOURCEVM=CentOS7-minimal
+set TARGETVM=Linked-Clone06
+set SNAPSHOT=latest
+"%VMRUNPATH%vmrun.exe" -T ws clone "%VMDIR%\%SOURCEVM%\%SOURCEVM%.vmx" "%VMDIR%\%TARGETVM%\%TARGETVM%.vmx" linked -cloneName="%TARGETVM%" -snapshot="%SNAPSHOT%"
+"%VMRUNPATH%vmrun.exe" -T ws start "%VMDIR%\%TARGETVM%\%TARGETVM%.vmx"
