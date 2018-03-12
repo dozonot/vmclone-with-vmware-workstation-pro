b6f794cb96ec8c7dfb0515e2f296d9c7e8cb8aa2
dozono <31640715+dozonot@users.noreply.github.com>
2018-03-12 18:43:02 +0900

    Update vmclone.bat
    immediate variable name

--- vmclone.bat
+++ vmclone.bat
@@ -1,7 +1,7 @@
 set VMRUNPATH=C:\Program Files (x86)\VMware\VMware Workstation\
 set VMDIR=%userprofile%\Documents\Virtual Machines
 set SOURCEVM=CentOS7-minimal
-set TARGETVM=CnetOS-01
 set SNAPSHOT=latest
+set TARGETVM=CentOS-01
 "%VMRUNPATH%vmrun.exe" -T ws clone "%VMDIR%\%SOURCEVM%\%SOURCEVM%.vmx" "%VMDIR%\%TARGETVM%\%TARGETVM%.vmx" linked -cloneName="%TARGETVM%" -snapshot="%SNAPSHOT%"
 "%VMRUNPATH%vmrun.exe" -T ws start "%VMDIR%\%TARGETVM%\%TARGETVM%.vmx"
